a070c4f19029275391ec2c2b502e5d96e33f92ca
Matias Luna <matias.luna@codetria.com>
2022-04-16 16:33:43 -0300

    Primer commit
    Co-authored-by: Rodrigo Franco <francorodrigo@users.noreply.github.com>

---
+++ Ejecutar.bat
@@ -0,0 +1,17 @@
+SET "flex_path=E:\GnuWin32\bin\flex"
+SET "bison_path=E:\GnuWin32\bin\bison"
+SET "mingw_gcc_path=E:\MinGW\bin\gcc.exe"
+%flex_path% %~d0%~p0Lexico.l
+pause
+%bison_path% -dyv %~d0%~p0Sintactico.y
+pause
+%mingw_gcc_path% lex.yy.c y.tab.c -o %~d0%~p0Primera.exe
+pause
+pause
+%~d0%~p0Primera.exe prueba.txt
+del lex.yy.c
+del y.tab.c
+del y.output
+del y.tab.h
+@REM del .\Primera.exe
+pause
