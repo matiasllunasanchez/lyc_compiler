f7dcefe0857a03dcc42c4e8b13605673ec2a315d
Matias Luna <matias.luna@codetria.com>
2022-05-14 16:31:49 -0300

    Se crea bloque para pruebas Se corren archivos de prueba a carpeta Se crean carpeta para sentencias de control y tercetos

--- Ejecutar.bat
+++ Ejecutar.bat
@@ -1,6 +1,6 @@
-SET "flex_path=E:\GnuWin32\bin\flex"
-SET "bison_path=E:\GnuWin32\bin\bison"
-SET "mingw_gcc_path=E:\MinGW\bin\gcc.exe"
+SET "flex_path=C:\GnuWin32\bin\flex"
+SET "bison_path=C:\GnuWin32\bin\bison"
+SET "mingw_gcc_path=C:\MinGW\bin\gcc.exe"
 %flex_path% "%~d0%~p0Lexico.l"
 pause
 %bison_path% -dyv "%~d0%~p0Sintactico.y"
@@ -9,3 +9,5 @@ pause
 pause
 "%~d0%~p0Primera.exe" prueba.txt
 pause
+@REM "%~d0%~p0Primera.exe" %~d0%~p0\tests\prueba_ok.txt
+@REM pause
