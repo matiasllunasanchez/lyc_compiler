047ad4a8c63c4762030804f65e300faa36d4040a
Matias Luna <matias.luna@codetria.com>
2022-05-21 19:42:11 -0300

    Se cambia nombre de ejecutable

--- Ejecutar.bat
+++ Ejecutar.bat
@@ -5,9 +5,7 @@ SET "mingw_gcc_path=C:\MinGW\bin\gcc.exe"
 pause
 %bison_path% -dyv "%~d0%~p0Sintactico.y"
 pause
-%mingw_gcc_path% lex.yy.c y.tab.c "libs/tabla_simbolos/tabla_simbolos.c" "libs/tercetos/tercetos.c" -std=gnu99 -o "%~d0%~p0Primera.exe"
+%mingw_gcc_path% lex.yy.c y.tab.c "libs/tabla_simbolos/tabla_simbolos.c" "libs/tercetos/tercetos.c" -std=gnu99 -o "%~d0%~p0Segunda.exe"
 pause
 "%~d0%~p0Segunda.exe" prueba.txt
 pause
-@REM "%~d0%~p0Primera.exe" %~d0%~p0\tests\prueba_ok.txt
-@REM pause
