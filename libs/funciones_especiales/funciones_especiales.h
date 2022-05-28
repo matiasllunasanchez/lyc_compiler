9ef86d49ef1f9b382253025c10db5cb6c576a57c
Matias Luna <matias.luna@codetria.com>
2022-05-28 11:09:01 -0300

    Se quitan variables innecesarias

--- libs/funciones_especiales/funciones_especiales.h
+++ libs/funciones_especiales/funciones_especiales.h
@@ -3,14 +3,11 @@
 #include "../sentencias_control/sentencias_control.h"
 
 
+// BETWEEN
 // Agrega indices a los tercetos que branchean / saltan al final de la sentencia between. Al flow final.
 // Tambien rellena el salto por falso general
 void poner_salto_hacia_fin_between(int idx_terceto_falso, int idx_salto_between, int idx_between, int idx_salto_a_fin_between_cota_inf, int idx_salto_a_fin_between_cota_sup);
 
-//Saltos y pila de between
-extern int ind_salto_inlist;
-extern int ind_inlist;
-extern int ind_inlist_a;
-extern int between_vector[MAX_ANIDAMIENTOS];
-extern int contador_inlist;
+// TAKE
+
 #endif
